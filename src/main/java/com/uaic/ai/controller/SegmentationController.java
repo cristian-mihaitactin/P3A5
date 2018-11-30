02fb3e70ef3b8141bbb9db2808571f236fa8a4e2
XKoutax <carolsfartz@gmail.com>
2018-11-30 13:21:34 +0200

    added Interface classes in Controller

--- src/main/java/com/uaic/ai/controller/SegmentationController.java
+++ src/main/java/com/uaic/ai/controller/SegmentationController.java
@@ -1,9 +1,15 @@
 package com.uaic.ai.controller;
 
+import com.uaic.ai.service.BinaryMatrix;
+import com.uaic.ai.service.ColumnsRecognition;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/segmentation")
 public class SegmentationController {
+
+    private BinaryMatrix binaryMatrix;
+    private ColumnsRecognition columnsRecognition;
+
 
 }
--- src/main/java/com/uaic/ai/model/Statistics.java
+++ src/main/java/com/uaic/ai/model/Statistics.java
@@ -2,4 +2,10 @@ package com.uaic.ai.model;
 
 public class Statistics {
 
+    /**
+     * TODO: add image properties for identifying columns, footnotes , header, text details, percentages,
+     *
+      */
+
+
 }
